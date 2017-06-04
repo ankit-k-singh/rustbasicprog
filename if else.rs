6467303be2868ca69d29bee8695464225e609c94
Ankitkumar Singh <ankit-k-singh@users.noreply.github.com>
2017-06-04 12:29:46 +0530

    Create if else.rs

---
+++ if else.rs
@@ -0,0 +1,27 @@
+
+fn main()
+{
+	let x = 5;
+
+
+
+if x == 5 
+	{
+		let y=7;
+
+		if y==6{
+				println!("And Y is six!");
+			}
+
+		else {
+			println!("y is not six!");
+		     }
+	} else {
+	    println!("x is not five :(");
+	}
+}
+
+
+
+
+
